4bee71f4b1705ebc4167814f6073935f51de8733
Kaushiik A <akkaushiik@gmail.com>
2025-12-27 22:14:16 +0530

    CI Test

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000, '0.0.0.0');
-  console.log(`🚀 Server running on http://localhost:3000`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Server running on port ${port}`);
 }
 bootstrap();
